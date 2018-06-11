47286db51237c9238312021f4a412114fd67b1c3
Adeline <adeline.contact@gmail.com>
2018-06-11 14:28:37 +0200

    Exercice03

--- exercicesjavascript.js
+++ exercicesjavascript.js
@@ -2,10 +2,19 @@
 // var toto = 'Bonjour';
 // alert(toto);
 
-Exercice02
+// Exercice02
+// var nom = "Doe";
+// var prenom = "John";
+// var ville = "New York";
 
-var nom = "Doe";
-var prenom = "John";
-var ville = "New York";
+// alert('Nom : '+nom+'\nPrénom : '+prenom+'\nVille : '+ville);
 
-alert('Nom : '+nom+'\nPrénom : '+prenom+'\nVille : '+ville);
+// Exercice03
+
+var rep = prompt('Quel est votre prénom?','Votre réponse ici');
+if (rep == null) {
+	alert('Vous avez tapé sur Annuler');
+} else {
+	alert('Bonjour '+rep);
+}
+
