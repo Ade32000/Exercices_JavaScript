dc646fa2578f666de0b73420fa38e4618f51446b
Adeline <adeline.contact@gmail.com>
2018-06-11 16:43:15 +0200

    Exercice 01 Exercice 02 Exercice 03 Exercice 04

--- exercicesjavascript.js
+++ exercicesjavascript.js
@@ -10,11 +10,39 @@
 // alert('Nom : '+nom+'\nPrénom : '+prenom+'\nVille : '+ville);
 
 // Exercice03
+// var rep = prompt('Quel est votre prénom?','Votre réponse ici');
+// if (rep == null) {
+// 	alert('Vous avez tapé sur Annuler');
+// } else {
+// 	alert('Bonjour '+rep);
+// }
 
-var rep = prompt('Quel est votre prénom?','Votre réponse ici');
-if (rep == null) {
-	alert('Vous avez tapé sur Annuler');
-} else {
-	alert('Bonjour '+rep);
-}
+// Exercice04
+// var qtn1 = prompt('Quel est votre nom?','Votre réponse ici');
+// var qtn2 = prompt('Quel est votre prénom?','Votre réponse ici');
+// var qtn3 = prompt('Quel est votre ville?','Votre réponse ici');
 
+// qtn1;
+// if (qtn1 == null) {
+// 	alert('Vous avez tapé sur Annuler');
+// } else {
+// 	qtn2;
+// 	if (qtn2 == null) {
+// 		alert('Vous avez tapé sur Annuler');
+// 	} else {
+// 		qtn3;
+// 		if (qtn3 == null) {
+// 			alert('Vous avez tapé sur Annuler');	
+// 		} else {
+// 			alert('Nom : '+qtn1+'\nPrénom : '+qtn2+'\nVille : '+qtn3);
+// 		}
+// 	}
+// }
+
+
+var valid = document.getElementById('valider');
+var nom = document.getElementById('nom');
+var prenom = document.getElementById ('prenom');
+var ville = document.getElementById ('ville');
+
+alert('Nom : '+nom+'\nPrénom : '+prenom+'\nVille : '+ville);
