2ae9d0dbb637c9ac3626d139bd5082cc4beed1f5
Adeline <adeline.contact@gmail.com>
2018-06-11 14:09:20 +0200

    Ajout exercice02

--- exercicesjavascript.js
+++ exercicesjavascript.js
@@ -1,2 +1,11 @@
-var toto = 'Bonjour';
-alert(toto);
+// Exercice01
+// var toto = 'Bonjour';
+// alert(toto);
+
+Exercice02
+
+var nom = "Doe";
+var prenom = "John";
+var ville = "New York";
+
+alert('Nom : '+nom+'\nPrénom : '+prenom+'\nVille : '+ville);
